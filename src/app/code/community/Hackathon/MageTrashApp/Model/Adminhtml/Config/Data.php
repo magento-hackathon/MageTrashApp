2bfde47debf5ed037484a46b167d5b407f2f42cd
Damian Luszczymak <damian.luszczymak@flagbit.de>
2013-01-12 15:34:01 +0100

    create Observer, rewrite adminhtml class for a possible bugfix for magento 1.7.0.2

---
+++ src/app/code/community/Hackathon/MageTrashApp/Model/Adminhtml/Config/Data.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: damian
+ * Date: 12.01.13
+ * Time: 15:31
+ * To change this template use File | Settings | File Templates.
+ */ 
+class Hackathon_MageTrashApp_Model_Adminhtml_Config_Data extends Mage_Adminhtml_Model_Config_Data {
+
+    // need to edit save
+}
--- src/app/code/community/Hackathon/MageTrashApp/Model/Observer.php
+++ src/app/code/community/Hackathon/MageTrashApp/Model/Observer.php
@@ -1,5 +1,9 @@
 <?php
 class Hackathon_MageTrashApp_Model_Observer extends Mage_Core_Model_Abstract {
 
+        public function test($observer) {
+            Mage::log('Damian');
+        }
+
 
 }
