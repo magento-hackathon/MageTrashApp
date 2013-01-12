80a17e25857d777cbd674c271e4b5673baf7f1f3
wsakaren <karen@webshopapps.com>
2013-01-12 16:08:23 +0100

    Started implementation of Observer to help new starter Damian

--- src/app/code/community/Hackathon/MageTrashApp/Model/Observer.php
+++ src/app/code/community/Hackathon/MageTrashApp/Model/Observer.php
@@ -1,8 +1,29 @@
 <?php
 class Hackathon_MageTrashApp_Model_Observer extends Mage_Core_Model_Abstract {
 
-        public function test($observer) {
+        public function saveConfig($observer) {
             Mage::log('Damian');
+
+            $modules = array_keys((array)Mage::getConfig()->getNode('modules')->children());
+
+            $dispatchResult = new Varien_Object($modules);
+
+            $modules = $dispatchResult->toArray();
+
+            foreach ($modules as $moduleName) {
+                if ($moduleName==='Mage_Adminhtml'  ||$moduleName==='Hackathon_MageTrashApp'
+                    || stripos($moduleName,'Mage_') !== false) {
+                    continue;
+                }
+
+                $configFlag = Mage::getStoreConfigFlag('magetrashapp/manage_extns/' . $moduleName);
+
+
+                Mage::log($configFlag);
+
+            }
+
+
         }
 
 
