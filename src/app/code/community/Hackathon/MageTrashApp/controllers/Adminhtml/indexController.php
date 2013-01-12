b1ce70ca8461f26d2ed14674cb4308e661c95ef3
Sylvain Rayé <sly@diglin.com>
2013-01-12 14:23:56 +0100

    Add basic logic for backend controllers and starting method to implements

---
+++ src/app/code/community/Hackathon/MageTrashApp/controllers/Adminhtml/indexController.php
@@ -0,0 +1,26 @@
+<?php
+
+class Hackathon_MageTrashApp_Adminhtml_indexController extends Mage_Core_Controller_Front_Action
+{
+	public function uninstallAction ()
+	{
+		$moduleName = $this->getRequest()->getParam('module_name');
+		
+		try {
+			Mage::helper()->uninstallModule($moduleName);
+		} catch (Exception $e) {
+			
+		}
+	}
+	
+	
+	public function activateModule ()
+	{
+		
+	}
+	
+	public function deactivateModule ()
+	{
+		
+	} 
+}
